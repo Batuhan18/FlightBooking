a72f6cbc2db0a414d421ea0fd57e2045c8478803
Ahmet Batuhan Avcıoğlu <54512066+Batuhan18@users.noreply.github.com>
2026-04-18 21:11:15 +0300

    ...

---
+++ FlightBooking/ViewComponents/BookingViewComponents/_BookingTagsViewComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.ViewComponents.BookingViewComponents
+{
+    public class _BookingTagsViewComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
