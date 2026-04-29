4dc7adf95ecc4f59f1cb9aaf9373479aca485ace
Ahmet Batuhan Avcıoğlu <54512066+Batuhan18@users.noreply.github.com>
2026-04-29 23:44:09 +0300

    ...

--- FlightBooking/Areas/Admin/Controllers/BookingController.cs
+++ FlightBooking/Areas/Admin/Controllers/BookingController.cs
@@ -1,4 +1,6 @@
-﻿using FlightBooking.Services.FlightServices;
+﻿using FlightBooking.Dtos.BookingDtos;
+using FlightBooking.Services.BookingServices;
+using FlightBooking.Services.FlightServices;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FlightBooking.Areas.Admin.Controllers
@@ -7,15 +9,20 @@ namespace FlightBooking.Areas.Admin.Controllers
     public class BookingController : Controller
     {
         private readonly IFlightService _flightService;
+        private readonly IBookingService _bookingService;
 
-        public BookingController(IFlightService flightService)
+        public BookingController(IFlightService flightService, IBookingService bookingService)
         {
             _flightService = flightService;
+            _bookingService = bookingService;
         }
 
+
+        [HttpGet]
         public async Task<IActionResult> CreateBooking(string id)
         {
             var value = await _flightService.GetFlightByIdAsync(id);
+            ViewBag.id = id;
             ViewBag.flightNumber = value.FlightNumber;
             ViewBag.departureAirportCode = value.DepartureAirportCode;
             ViewBag.departureAirportName = value.DepartureAirportName;
@@ -23,7 +30,15 @@ namespace FlightBooking.Areas.Admin.Controllers
             ViewBag.arrivaleAirportName = value.ArrivalAirportName;
             ViewBag.departureTime = value.DepartureTime;
             ViewBag.arrivalTime = value.ArrivalTime;
+            ViewBag.airlaneCode = value.AirlineCode;
             return View(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBooking(CreateBookingDto createBookingDto)
+        {
+            await _bookingService.CreateBookingAsync(createBookingDto);
+            return RedirectToAction("Index", "Bookings", new { area = "Admin" });
         }
 
         public IActionResult BookingList()
